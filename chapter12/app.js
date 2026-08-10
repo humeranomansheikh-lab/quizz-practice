d08a9c81e3f3f88923bbd648c207dd36cf472103
humeranomansheikh-lab <humeranomansheikh@gmail.com>
2026-08-10 09:11:42 -0700

    12 complete

--- chapter12/app.js
+++ chapter12/app.js
@@ -1,22 +1,218 @@
-var character = prompt("write any character")
-var asciicode = character.charCodeAt()
-console.log(asciicode)
+// var character = prompt("write any character")
+// var asciicode = character.charCodeAt()
+// console.log(asciicode)
 
-if(asciicode>=48 && asciicode<=57){
-    alert("it is a number")
+// if(asciicode>=48 && asciicode<=57){
+//     alert("it is a number")
+// }
+
+//    else if(asciicode>=97 && asciicode<=122){
+//     alert("it is a lower case letter")
+// }
+
+//      else if(asciicode>=65 && asciicode<=90){
+//     alert("it is a upper case letter")
+
+
+
+
+// }
+// else{
+//     alert("invalid number")
+// }
+
+// QUES2
+
+// if(5>4){
+//     console.log(5 + "is larger than" + 4)
+// }
+// else if(7>2){
+//     console.log(7 + "is larger than + 2")
+// }
+// else{
+//     console.log("both integers are equal")
+// }
+// ques 3
+
+// var number = prompt("enter a number")
+// if(number>0){
+//     alert("its apositive number")
+
+// }
+// else if(number<0){
+//     alert("its a negative number")
+// }
+
+// else{
+//     alert("the number is zero")
+// }
+
+// ques4
+
+// var character = prompt("write a character to check whether its a vowel or not")
+
+
+// if (character == "A" || character == "a" || character == "E" || character == "e" || character == "I" || character == "i" || character == "O" || character == "o" || character == "U" || character == "u"){
+//     alert("its avowel")
+// }
+// else{
+//     alert("its not a vowel")
+// // }
+
+
+
+// ques 5
+// var correctPassword = "1234"
+// var password = prompt("enter your password")
+// if(password.length ==0){
+// alert("please enter your password")
+// }
+// else if(password === correctPassword){
+//     alert("correct")}
+//     else{
+//         alert("incorrect password")
+//     }
+
+
+
+// ques6
+
+
+var greeting;
+var hour = 13;
+
+if (hour>18){
+ greeting = "good day" ;           
 }
+else {
+    greeting = "good day" }
 
-   else if(asciicode>=97 && asciicode<=122){
-    alert("it is a lower case letter")
-}
 
-     else if(asciicode>=65 && asciicode<=90){
-    alert("it is a upper case letter")
-        
+    // ques7
 
-    
+    var time = 1900;
+    if (time>=0000);{
+        alert ("Good morning")
+    }
 
-}
-else{
-    alert("invalid number")
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
